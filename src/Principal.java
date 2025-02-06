5b92ded4ff65298be8c0dab4a12aad0fcf8bf481
Nayara <nayaramaisa.stn@gmail.com>
2025-02-06 10:40:20 -0300

    only change

---
+++ src/Principal.java
@@ -0,0 +1,18 @@
+/**
+ * @author nayara.maisa
+ */
+
+ public class Principal     {
+
+     public static void main(String args[]){
+         Numeros();
+     }
+    public static void Numeros(){
+       int numero1 = 56;
+       System.out.println(numero1);
+
+      short numero2 = (short) numero1;
+        System.out.println(numero2);
+    }
+
+}
